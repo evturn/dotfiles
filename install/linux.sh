1d4525010b3b7e049045be4c9244128e30af0db8
Evan Turner <ev@evturn.com>
2018-01-24 03:32:23 -0500

    Create separate OS bootstrap shell scripts

---
+++ install/linux.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Linux rsync
+
+echo "Updating Linux dotfiles";
+
+rsync --exclude ".git/" \
+      --exclude ".gitmodules" \
+      --exclude "init/" \
+      --exclude "install/" \
+      --exclude "nvim/" \
+      --exclude "vim/" \
+      --exclude ".macos" \
+      --exclude ".bash_profile" \
+      --exclude ".path" \
+      --exclude "bootstrap.sh" \
+      --exclude "README.md" \
+      -avh --no-perms . ~;
+source ~/.profile;
