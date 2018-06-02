413a8599f1114c304e3b234243d644604afb4512
Evan Turner <ev@evturn.com>
2018-06-01 22:23:22 -0400

    Add config for IPython

---
+++ ipython/ipython_config.py
@@ -0,0 +1,9 @@
+c = get_config()
+
+c.TerminalIPythonApp.display_banner = False
+
+c.InteractiveShell.autoindent   = True
+c.InteractiveShell.colors       = 'Linux'
+c.InteractiveShell.confirm_exit = False
+c.InteractiveShell.editor       = 'nvim'
+c.InteractiveShell.xmode        = 'Plain'
