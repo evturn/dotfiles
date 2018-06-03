6f47fd9321020f83b94899578723ba68bb3283c2
Evan Turner <ev@evturn.com>
2018-06-02 22:23:22 -0400

    Add symlink operations to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,33 @@
 
 cd "$(dirname "${BASH_SOURCE}")" 
 
+function createSymlink() {
+  if [ ! -L $2 ]; then
+    ln -s $1 $2
+    echo "Link created: $2"
+  fi
+}
+
+SRC_IPYTHON="${PWD}/ipython/ipython_config.py"
+CFG_IPYTHON="${HOME}/.ipython/profile_default/ipython_config.py"
+SRC_NVIM="${PWD}/nvim/"
+CFG_NVIM="${HOME}/.config/nvim"
+SRC_VIM="${PWD}/vim/"
+CFG_VIM="${HOME}/.vim"
+SRC_VIMRC="${PWD}/vimrc"
+CFG_VIMRC="${HOME}/.vimrc"
+SRC_GVIMRC="${PWD}/vim/gvimrc"
+CFG_GVIMRC="${HOME}/.gvimrc"
+SRC_BASH="${PWD}/settings/"
+CFG_BASH="${HOME}/settings"
+
+createSymlink $SRC_IPYTHON $CFG_IPYTHON
+createSymlink $SRC_NVIM $CFG_NVIM
+createSymlink $SRC_VIM $CFG_VIM
+createSymlink $SRC_VIMRC $CFG_VIMRC
+createSymlink $SRC_GVIMRC $CFG_GVIMRC
+createSymlink $SRC_BASH $CFG_BASH
+
 rsync --include './' \
       --include '.bash_profile' \
       --include '.bashrc' \
