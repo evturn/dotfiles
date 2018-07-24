277d97c9ff6ac6abc5a824de4889b42a43e8700b
Evan Turner <ev@evturn.com>
2018-07-23 22:23:22 -0400

    Update ipython config

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -1,13 +1,14 @@
 c = get_config()
 
-c.TerminalIPythonApp.display_banner = False
-c.TerminalInteractiveShell.editing_mode = 'vi'
-
 c.InteractiveShell.autoindent= True
 c.InteractiveShell.colors = 'Linux'
 c.InteractiveShell.confirm_exit = False
-c.InteractiveShell.editor = 'nvim'
+c.InteractiveShell.editor = 'vim'
+c.InteractiveShell.xmode = 'Context'
 
 c.InteractiveShellApp.exec_lines = ['%reload_ext autoreload', '%autoreload 2']
 c.InteractiveShellApp.extensions = ['autoreload']
-c.InteractiveShell.xmode = 'Plain'
+
+c.TerminalInteractiveShell.editing_mode = 'vi'
+
+c.TerminalIPythonApp.display_banner = False
