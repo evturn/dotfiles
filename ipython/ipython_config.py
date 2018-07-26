d420a16ac624065e12c0d17a864b5bc9af9f44dc
Evan Turner <ev@evturn.com>
2018-07-25 22:23:22 -0400

    Override IPython highlighting styles

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -1,3 +1,4 @@
+from pygments.token import Token
 c = get_config()
 
 c.InteractiveShell.autoindent= True
@@ -10,5 +11,20 @@ c.InteractiveShellApp.exec_lines = ['%reload_ext autoreload', '%autoreload 2']
 c.InteractiveShellApp.extensions = ['autoreload']
 
 c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.highlighting_style_overrides = {
+    Token.Keyword: '#ff0087',
+    Token.Keyword.Constant: '#0087ff',
+    Token.Keyword.Reserved: '#ff0087',
+    Token.Keyword.Type: '#5fd7ff',
+    Token.Name: '#00ffff',
+    Token.Name.Builtin: '#5fd7ff',
+    Token.Name.Class: '#d7ff00',
+    Token.Name.Constant: '#00ffff',
+    Token.Name.Function: '#d7ff00',
+    Token.Literal.String: '#00ffaf',
+    Token.Literal.Number: '#af87ff',
+    Token.Operator: '#ff0087',
+    Token.Punctuation: '#ff0000',
+}
 
 c.TerminalIPythonApp.display_banner = False
